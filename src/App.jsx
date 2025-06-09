4eaa59d1194573b53822df7e9b8242251c4935f0
Emilse Arias <emilsearias@gmail.com>
2025-06-09 20:33:04 -0300

    agregamos el maquetado del componente Información

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Informacion from "./components/Informacion";
 
 function App() {
-//aqui va la logica del componente
+  //aqui va la logica del componente
 
   return (
     // aqui puedo agregar algo de logica
-    <>
-    {/* Aqui va todo el maquetado html */}
-      <h1>Hola mundo</h1>
-      <Informacion/>
-    </>
+    <main className="container my-5">
+      {/* Aqui va todo el maquetado html */}
+      <h1>Hola mundo 2</h1>
+      <Informacion />
+    </main>
   );
 }
 
--- src/components/Informacion.jsx
+++ src/components/Informacion.jsx
@@ -1,9 +1,20 @@
 const Informacion = () => {
-    return (
-        <div>
-            <h2>Primer proyecto con react</h2>
-        </div>
-    );
+  return (
+    <section>
+      <h2>Primer proyecto con react</h2>
+      <p>
+        React es una biblioteca de JavaScript desarrollada por Facebook para
+        construir interfaces de usuario. <br/>Se destaca por su enfoque en la
+        creación de aplicaciones web de una sola página SPA y su capacidad para
+        manejar la interfaz de manera eficiente mediante el uso de un <b>DOM virtual</b>.
+      </p>
+      <p>
+        React permite a los desarrolladores construir interfaces dinámicas y
+        reactivas de forma modular, lo que facilita el desarrollo y el
+        mantenimiento de aplicaciones a gran escala.
+      </p>
+    </section>
+  );
 };
 
 export default Informacion;

b293953789e98368dacd5b66dbea5ba061a7fc9d
Emilse Arias <emilsearias@gmail.com>
2025-06-09 21:34:50 -0300

    actualizamos la funcion de sumar para que use el state numero

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
+
 const Contador = () => {
+//crear un estado 
+const [numero, setNumero] = useState(1)
+//let numero = 1
+const sumar = ()=>{
+    setNumero(numero + 1)
+    console.log(numero)
+}
+
     return (
         <section className="text-center mt-5">
             <h2 >Contador</h2>
-            <h3>Nº: 1</h3>
-            <button className="btn btn-primary me-3">+</button>
+            <h3>Nº: {numero}</h3>
+            <button className="btn btn-primary me-3" onClick={sumar}>+</button>
             <button className="btn btn-danger">-</button>
         </section>
     );
